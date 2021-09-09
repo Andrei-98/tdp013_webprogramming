3f0d3a52512d58ba943f335af26ede263629140b
kasni325 <kasni325@student.liu.se>
2021-09-09 16:00:46 +0200

    read

--- lab_1/index.js
+++ lab_1/index.js
@@ -1,3 +1,6 @@
+
+let mess_arr = 0;
+
 function render_message()
 {
     let text = document.getElementById("text_input").value;
@@ -21,22 +24,29 @@ function main()
     if(mess != "")
     {
         let mess_arr = mess.split(";");
-        console.log(mess_arr.length);
+
+        let sorted_arr = [];
 
         for(let i = 0; i < mess_arr.length; i++)
         {
             mess_arr[i] = mess_arr[i].replace(/message\d+\=/, "");
-            display_message(mess_arr[i]);
+            sorted_arr.push(JSON.parse(mess_arr[i]));
+        }
+
+        sorted_arr.sort((a,b) => parseInt(a.id) > parseInt(b.id));    
+
+        for(let i = 0; i < sorted_arr.length; i++)
+        {
+            display_message(sorted_arr[i].content, sorted_arr[i].isRead);
         }
     }
+
 }
 
-function save_message(text)
+function save_message(text, is_read = 0, id = mess_arr)
 {
-    let real_count = document.cookie.length;
-    console.log(real_count);
-
-    document.cookie = "message" + real_count + "=" + text + ";expires=Mon, 01 Nov 2021 12:00:00 UTC"; // TODO Is message read/unread.
+    let message_obj = {"id" : id, "content" : text.replace(/[\r\n]+$/, ''), "isRead" : is_read};
+    document.cookie = "message" + id + "=" + JSON.stringify(message_obj) + ";expires=Mon, 01 Nov 2021 12:00:00 UTC"; // TODO Is message read/unread
 }
 
 function error_msg()
@@ -51,7 +61,7 @@ function reset_error()
     error.textContent = "";
 }
 
-function display_message(text) // Kasper version
+function display_message(text, is_read = 0) // Kasper version
 {
     const list = document.getElementById("container-2");
     const new_msg = document.createElement("div");
@@ -59,33 +69,50 @@ function display_message(text) // Kasper version
 
     msg_btn.innerHTML = document.createTextNode("Read").textContent;
     msg_btn.setAttribute("class","box_checkbox");
-    msg_btn.setAttribute("name", "off");
-    msg_btn.addEventListener('click', read_message);
-
     const msg_content = document.createElement("p");
     msg_content.setAttribute("class", "box_text");
     msg_content.innerText = text;
+    if (msg_btn.getAttribute("id") == null)
+    {
+        msg_btn.setAttribute("id", mess_arr);
+    }
+    
+    if(is_read == 0)
+    {
+        msg_btn.setAttribute("name", "off");
+    }
+    else
+    {
+        msg_btn.setAttribute("name", "on");
+        msg_content.style.color = "grey";
+    }
+
+    msg_btn.addEventListener('click', read_message);
 
     new_msg.setAttribute("class", "msg_box");
     new_msg.appendChild(msg_content);
     new_msg.appendChild(msg_btn);
-         
+
 
     list.insertBefore(new_msg, list.childNodes[0]);
+    mess_arr += 1;
 }
+
 
 // listener - function for message buttons
 function read_message() 
 {
     if(this.getAttribute("name") == "off")
     {
-        this.setAttribute("name","on");
+        this.setAttribute("name", "on");
         this.previousSibling.style.color = "grey";
+        save_message(this.previousSibling.textContent, 1, parseInt(this.getAttribute("id")));
     }
     else
     {
-        this.setAttribute("name","off");
+        this.setAttribute("name", "off");
         this.previousSibling.style.color = "black";
+        save_message(this.previousSibling.textContent, 0, parseInt(this.getAttribute("id")));
     }
 }
 
