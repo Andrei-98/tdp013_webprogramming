d3eee192184bfe1f1c86d32a9da5803c2196a7c7
Andrei Plotoaga <andpl509@student.liu.se>
2021-10-01 11:51:20 +0200

    register component with Link and routes

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -1,13 +1,22 @@
 import { Component } from 'react';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Login />
-      </div>
+
+        <div className="App">
+          <Router>
+          <Switch>
+            <Route exact path="/">  <Login/> </Route>
+            <Route exact path="/register"> <Register/> </Route>
+          </Switch>
+          </Router>
+        </div>
+
     );
   }
 
--- project/client/src/components/Register.js
+++ project/client/src/components/Register.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 import Button from './Button';
-import { Link } from "react-router-dom";
+
 
 export class Login extends Component {
     render() {
         return (
             <div>
                 <form action='#' method="post" className="login">
-                    <input type="text" id="uname" placeholder="Username"></input>
-                    <input type="password" id="pword" placeholder="Password"></input>
+                    <input type="text" id="uname3" placeholder="Username"></input>
+                    <input type="password" id="pword1" placeholder="Password"></input>
+                    <input type="password" id="pword2" placeholder="Password"></input>
+
                     <Button />
                 </form>
-                <a><Link to="/register">  Register  </Link> </a>
-
             </div>
         )
     }
