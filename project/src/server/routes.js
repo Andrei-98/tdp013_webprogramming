51a407bea96dc1dcf15f5173f55fd884a6ddfe50
Andrei Plotoaga <andpl509@student.liu.se>
2021-10-05 09:23:01 +0200

    text area in /profile will send message to db

--- project/src/server/routes.js
+++ project/src/server/routes.js
@@ -2,6 +2,7 @@
 //  /profile/username
 //  /friends
 //  /chat
+let ObjectId = require('mongodb').ObjectID
 
 let express = require('express');
 let router = express.Router();
@@ -19,15 +20,15 @@ router.post("/", (req, rsp) => {
 
     // log in
 
-    const user = {username : req.body.username, password : req.body.password};
+    const user = { username: req.body.username, password: req.body.password };
     const status = dbHandler.sign_in(user);
     status.then((res) => {
-        if(res != null)
+        if (res != null)
             rsp.sendStatus(200);
         else
             rsp.sendStatus(401);
     })
-   
+
 });
 
 router.put("/profile", (req, rsp) => {
@@ -38,41 +39,53 @@ router.put("/profile", (req, rsp) => {
     dbHandler.send_friend_request(friend_requester, friend_target);
     rsp.sendStatus(200);
 });
-  
+
 router.post("/register", (req, rsp) => {
 
     // register
 
-    const user = {username : req.body.username, password : req.body.password,
-                "sent_req" : [], "received_req" : [],  "friends" : [], "messages" : []};
+    const user = {
+        username: req.body.username, password: req.body.password,
+        "sent_req": [], "received_req": [], "friends": [], "messages": []
+    };
     dbHandler.add_user(user);
     rsp.sendStatus(200);
 
-   
+
 });
 
 router.put("/profile/fr", (req, rsp) => {
 
     const friend_target = req.body.receiver;
     const friend_requester = req.body.sender;
-    
+
     dbHandler.accept_friend(friend_requester, friend_target);
-    rsp.sendStatus(200); 
+    rsp.sendStatus(200);
 
 });
-  
-router.get("/friends", (req,rsp) => {
+
+router.post("/profile", (req, rsp) => {
+    console.log("------------")
+    const content = req.body.content;
+    const from = req.body.from;
+    let message = { "content": content, "from": from }
+    dbHandler.insert_message(message, from)
+
+    rsp.sendStatus(200);
+});
+
+router.get("/friends", (req, rsp) => {
 
     // find friends
 });
 
-  
+
 router.get("/chat", (req, rsp) => {
     // chatting, if time
 });
-  
-  
-router.get("/profile", (req,rsp) => {
+
+
+router.get("/profile", (req, rsp) => {
     // profile 
 });
 
